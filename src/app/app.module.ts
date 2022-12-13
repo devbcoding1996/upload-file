9d577067899c70f53fea1ee22e904c513a5b4b23
weedanai <weedanai@hiluxsoft.com>
2022-12-13 10:49:41 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UploadFileComponent } from './upload-file/upload-file.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent, UploadFileComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatProgressBarModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,75 @@
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { finalize, Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.scss'],
+})
+export class UploadFileComponent implements OnInit {
+  public formGroup!: FormGroup;
+  public _file!: any;
+  public formData!: any;
+  uploadProgress!: any;
+  uploadSub!: Subscription;
+  total!: any;
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.formGroup = this.fb.group({
+      file: ['', [Validators.required]],
+    });
+  }
+
+  get file() {
+    return this.formGroup.controls['file'];
+  }
+
+  uploadFile(event: any) {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      if (Math.round(file.size / 1024) > 3000) {
+        Swal.fire(
+          'ตำเตือน',
+          `ไฟล์ของท่านมีขนาดใหญ่เกินไป ${Math.round(file.size / 1024) + ' KB'}`,
+          'error'
+        );
+      } else {
+        this._file = file.name;
+        this.formData = new FormData();
+        this.formData.append('controller', 'uploadFile');
+        this.formData.append('thumbnail', file);
+      }
+    }
+  }
+
+  handleUploadFle() {
+    console.log(this.formData);
+    const upload$ = this.http
+      .post('/api/thumbnail-upload', this.formData, {
+        reportProgress: true,
+        observe: 'events',
+      })
+      .pipe(finalize(() => this.reset()));
+
+    this.uploadSub = upload$.subscribe((event) => {
+      if (event.type == HttpEventType.UploadProgress) {
+        this.total = event.total;
+        this.uploadProgress = Math.round(100 * (event.loaded / this.total));
+      }
+    });
+  }
+
+  reset() {
+    this.uploadProgress = null;
+  }
+}
